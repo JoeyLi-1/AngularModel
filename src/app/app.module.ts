518c10d74b4f39be11f9e052653cd142d678dcc9
Taolue <1164734648@qq.com>
2020-03-27 20:42:49 +0800

    remove

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { SpotlightService } from './spotlight/spotlight.service';
-import { Spotlight } from './spotlight/spotlight.component';
-import { SpotlightHeader } from './spotlight/spotlightHeader/spotlightHeader.component';
-import { SpotlightHeaderService } from './spotlight/spotlightHeader/spotlightHeader.service';
-import { ValueFilterComponet } from './spotlight/valueFiilter/valueFilter.component';
-import { ValueFilterService } from './spotlight/valueFiilter/valueFilter.service';
-import { PaginationService } from './spotlight/pagination/pagination.service';
-import { PaginationComponent } from './spotlight/pagination/pagination.component';
+// import { SpotlightService } from './spotlight/spotlight.service';
+// import { Spotlight } from './spotlight/spotlight.component';
+// import { SpotlightHeader } from './spotlight/spotlightHeader/spotlightHeader.component';
+// import { SpotlightHeaderService } from './spotlight/spotlightHeader/spotlightHeader.service';
+// import { ValueFilterComponet } from './spotlight/valueFiilter/valueFilter.component';
+// import { ValueFilterService } from './spotlight/valueFiilter/valueFilter.service';
+// import { PaginationService } from './spotlight/pagination/pagination.service';
+// import { PaginationComponent } from './spotlight/pagination/pagination.component';
 
 
 import { SpotlightsComponent } from './spotlights/spotlights.component';
@@ -42,10 +42,10 @@ import { ValueService } from './spotlights/valueFitter/value.component.service';
     AppRoutingModule
   ],
   providers: [ // Service 
-    SpotlightService,
-    SpotlightHeaderService,
-    ValueFilterService,
-    PaginationService,
+    // SpotlightService,
+    // SpotlightHeaderService,
+    // ValueFilterService,
+    // PaginationService,
     SpotlightsService,
     PaginationsService,
     SpotlightsHeaderService,
