3105f680bf8f7aa8d5362f7a4237de081a1634f7
Joey Li <joey@clarifyhealth.com>
2022-09-26 18:03:21 +0800

    add tracking emit change history functionality

--- src/app/ReactComponentBase.ts
+++ src/app/ReactComponentBase.ts
@@ -1,12 +1,38 @@
-import { Input, Output, EventEmitter } from '@angular/core';
+import { Input, Output, EventEmitter, Directive, OnChanges, SimpleChanges, isDevMode } from '@angular/core';
 import * as _ from 'lodash';
 
-export class ReactComponentBase<P, S, O> {
+// This flag will output all the emit and change event of all component inherited from ReactComponentBase
+// Just for debugging use. It is only working under dev mode.
+const outputTrackingInfo = true;
+
+export class MyEventEmitter<T> extends EventEmitter<T> {
+    caller: any;
+    constructor(c: any) {
+        super();
+        this.caller = c;
+    }
+
+    public emit(data?: any): void {
+        super.emit(data);
+        if (isDevMode() && outputTrackingInfo) {
+            // One emit should not trigger more than one change event for same component
+            // otherwise, it could cause performance issue
+            console.log(`${this.caller.constructor.name} emitted a message`);
+        }
+    }
+}
+
+@Directive()
+export class ReactComponentBase<P, S, O> implements OnChanges {
     private _props: P;
     private _states: S;
     @Input('props')
     set props(val: P) {
-        this._props = _.merge(this._props, val);
+        this._props = _.mergeWith(this._props, val, (obj, src) => {
+            if (_.isArray(src)) {
+                return src;
+            }
+        });
     }
 
     get props(): P {
@@ -21,7 +47,7 @@ export class ReactComponentBase<P, S, O> {
         return this._states;
     }
 
-    @Output() changed: EventEmitter<O> = new EventEmitter(); 
+    @Output() changed: MyEventEmitter<O> = new MyEventEmitter(this);
 
     constructor(defaultStates?: S, defaultProps?: P) {
         this._props = {} as P;
@@ -31,6 +57,12 @@ export class ReactComponentBase<P, S, O> {
         }
         if (defaultStates) {
             this._states = defaultStates;
+        }
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (isDevMode() && outputTrackingInfo) {
+            console.log(`${this.constructor.name} received changes notification`);
         }
     }
 
