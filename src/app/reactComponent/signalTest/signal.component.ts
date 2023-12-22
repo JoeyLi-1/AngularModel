90b334ae53a56ff0ffad3d283e2282d4313891c6
Joey Li <joey@clarifyhealth.com>
2023-12-22 15:25:55 +0800

    refactor signal example

--- src/app/reactComponent/signalTest/signal.component.ts
+++ src/app/reactComponent/signalTest/signal.component.ts
@@ -1,85 +1,64 @@
-import { Component, OnChanges, SimpleChanges, OnInit, signal, computed } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 
-interface Metric {
-    id: string;
+interface Member {
+    id: number;
     name: string;
     value: number;
-    display: boolean;
 }
 
 @Component({
     template: `<div>
-        <p>{{count().name}} -- {{count().value}}</p>
-        <p>{{count2()}}</p>
-        <p>id: {{count().id}}</p>
+        <p>{{targetsSignal()[0].name}} has {{targetsSignal()[0].value}}</p>
         <button (click)="increase()">Increase</button>
+        
         <button (click)="decrease()" [disabled]="canDecrease()">Decrease</button>
-        <div *ngFor="let name of namesFiltered()">{{ name }}</div>
-        <div
-            style="height: 300px; width: 300px; background-color: #2f487e;"
-            (mousemove)="onMousemove()">
-        </div>
-        <test-signal-sub [metric]="count()"></test-signal-sub>
+        <p>Name starts with M</p>
+        <div *ngFor="let member of computedNames()">{{member.id}} - {{member.name}}</div>
+        
+        <div style="height: 300px; width: 300px; background-color: #2f487e;" (mousemove)="onMousemove()"></div>
+        <test-signal-sub [members]="targetsSignal()"></test-signal-sub>
     </div>`,
     selector: 'test-signal',
 })
 
 export class SignalComponent {
-    names = ['Maria', 'Michel', 'Jack', 'John', 'Sam', 'Mila'];
-    namesSignals = signal(['Maria', 'Michel', 'Jack', 'John', 'Sam', 'Mila']);
-    readonly count = signal<Metric>({
-        id: '1',
-        name: 'a',
-        value: 1,
-        display: true
-    });
-
-    count2 = computed(() => {
-        return this.count().value * 2;
-    });
-
-    disableDecrease = computed(() => {
-        this.counter1++;
-        console.log("disableDecrease() called! " + this.counter1 + " times!");
-        return this.count().value <= 0;
-    });
-
     counter = 0;
     counter1 = 0;
 
-    namesFiltered = computed(() => {
+    targets: Member[] = [{name: 'Maria', id: 1, value: 1}, {name: 'Michel', id: 2, value: 1}, {name: 'Jack', id: 3, value: 1}, {name: 'John', id: 4, value: 1}, 
+                {name: 'Sam', id: 5, value: 1}, {name: 'Mila', id: 6, value: 1}];
+    readonly targetsSignal = signal<Member[]>(this.targets);
+    
+    computedNames = computed(() => {
         this.counter++
         console.log("namesFiltered() called! " + this.counter + " times!");
-        return this.namesSignals().filter(name => name.startsWith('M'));
+        return this.targetsSignal().filter(item => item.name.startsWith('M'));
     })
 
     constructor() {
 
     }
 
-    filterNames(): string[] {
-        this.counter++
-        console.log("filterNames() called! " + this.counter + " times!");
-        return this.names.filter(name => name.startsWith('M'));
-    }
-
     onMousemove() {
-        console.log('Mouse moved');
+        // console.log('Mouse moved');
     }
 
     increase() {
-        this.count.update(val => { return {...val, value: val.value + 1 }; });
-        // this.count().value += 1;
+        const newTargets = [].concat(this.targetsSignal());
+        newTargets[0].value += 1;
+        this.targetsSignal.set(newTargets);
     }
 
     decrease() {
-        this.count.update(val => { return {...val, value: val.value - 1 }; });
-        // this.count().value -= 1;
+        this.targetsSignal.update(val => { 
+            val[0].value -= 1;
+            return val;
+        });
     }
 
     canDecrease() {
         this.counter1++;
         console.log("canDecrease() called! " + this.counter1 + " times!");
-        return this.count().value <= 0;
+        return this.targetsSignal()[0].value <= 0;
     }
 }
--- src/app/reactComponent/signalTest/signalSub.component.ts
+++ src/app/reactComponent/signalTest/signalSub.component.ts
@@ -1,30 +1,29 @@
-import { Component, OnChanges, SimpleChanges, OnInit, signal, computed, Input } from '@angular/core';
-
-interface Metric {
-    id: string;
-    name: string;
-    value: number;
-    display: boolean;
-}
+import { Component, OnChanges, SimpleChanges, Input } from '@angular/core';
 
 @Component({
     template: `<div>
-        This is sub signal
-        <p>{{member}}</p>
+        <p>Sub signal component below:</p>
+        <p>{{name}} preferred {{color}}</p>
+        <p>{{members[0].name}} has value {{members[0].value}}</p>
     </div>`,
     selector: 'test-signal-sub',
 })
 
 export class SignalSubComponent implements OnChanges {
-    @Input() metric: any;
-    member = 0;
+    @Input() members: any;
+    color = '';
+    name = '';
+    readonly preferedColor = [{name: 'Maria', color: 'red'}, {name: 'Michel', color: 'blue'}, {name: 'Jack', color: 'yellow'}, {name: 'John', color: 'green'}, 
+    {name: 'Sam', color: 'black'}, {name: 'Mila', color: 'white'}];
     constructor() {
 
     }
 
     ngOnChanges(changes: SimpleChanges): void {
-        if (changes.metric?.previousValue !== changes.metric?.currentValue) {
-            this.member = changes.metric.currentValue.value;
+        if (changes.members?.previousValue !== changes.members?.currentValue) {
+            const index = changes.members?.currentValue[0].value % changes.members?.currentValue.length ?? 0;
+            this.color = this.preferedColor[index].color;
+            this.name = this.preferedColor[index].name;
         }
     }
 }
